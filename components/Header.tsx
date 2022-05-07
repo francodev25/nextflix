bd27d85f343e205da109a1df045cd4bec8b09ed5
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-05-07 19:16:14 -0300

    Header Component Styled

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,30 @@
-import {SearchIcon } from '@heroicons/react/solid'
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+  useEffect(() => {
+    const handleScroll = () =>
+      window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false)
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, [])
+
+  const headerClassName = isScrolled ? 'bg-[#141414]': ' '
+
   return (
-    <header>
+    <header className={headerClassName}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
           width={100}
           height={100}
-          className='cursor-pointer object-contain'
-
+          className="cursor-pointer object-contain"
         />
         <ul className="hidden space-x-4 md:flex">
           <li className="headerLink">Home</li>
@@ -22,6 +37,15 @@ function Header() {
 
       <div className="flex items-center space-x-4 text-sm font-light">
         <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">KIDS</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   )
