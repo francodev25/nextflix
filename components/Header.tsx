f70ee8157ec7f20bfff55656851f965b81b31d5f
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-05-07 17:37:50 -0300

    Install Heroicons | 1st Approach Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,29 @@
+import {SearchIcon } from '@heroicons/react/solid'
+
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          width={100}
+          height={100}
+          className='cursor-pointer object-contain'
+
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">Tv Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
+      </div>
+    </header>
   )
 }
 
