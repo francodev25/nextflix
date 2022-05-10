a3eb67f1ad724be29dcb3f9bdf966cc77d128a38
Franco Narvaez <juanfranconarvaez@gmail.com>
2022-05-09 21:50:17 -0300

    Banner First Approach

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next'
+import requests from '../utils/requests'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Banner from '../components/Banner'
+import { Movie } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[]
+}
+
+const Home = ({netflixOriginals}: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -12,6 +19,8 @@ const Home: NextPage = () => {
       <Header/>
       <main>
         {/** Banner */}
+        <Banner netflixOriginals={netflixOriginals} />
+
         <section>
           {/** Row */}
           {/** Row */}
@@ -25,3 +34,39 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
+}
